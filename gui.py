329b636e68793c3d9a68c09747ca3232c977dfb5
murphymy04 <137734306+murphymy04@users.noreply.github.com>
2023-07-11 21:00:25 -0500

    RecipeSearch class
    -calls API with all parameters which come from user
    -basic gui setup within gui.py

---
+++ gui.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+class GUI:
+    
+
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Recipy")
+        self.window.mainloop()
+
+gui = GUI()
+
---
+++ recipe_search.py
@@ -0,0 +1,50 @@
+from dotenv import load_dotenv
+import os
+import requests
+
+
+class RecipeSearch:
+    
+    
+    def __init__(self):
+        load_dotenv()
+        self.RECIPE_ID = os.getenv("recipe_id")
+        self.RECIPE_KEY = os.getenv("recipe_key")
+        self.PARAMS = {
+            "type": "public",
+            "app_id": self.RECIPE_ID,
+            "app_key": self.RECIPE_KEY,
+            "random": True
+        }
+
+
+    def query(self, input):
+        self.PARAMS["q"] = input
+
+    
+    def diet(self, diet_input):
+        self.PARAMS["diet"] = diet_input
+
+    
+    def health(self, health_input):
+        self.PARAMS["health"] = health_input
+
+
+    def cuisine_type(self, cuisine_input):
+        self.PARAMS["cuisineType"] = cuisine_input
+    
+
+    def meal_type(self, meal_input):
+        self.PARAMS["mealType"] = meal_input
+
+
+    def search(self):
+        self.response = requests.get("https://api.edamam.com/api/recipes/v2", params=self.PARAMS)
+        self.response.raise_for_status()
+        self.data = self.response.json()
+
+
+#response = requests.get("https://api.edamam.com/api/recipes/v2", params=PARAMS)
+#response.raise_for_status()
+#data = response.json()
+#print(data["hits"][0]["recipe"]["ingredientLines"])
