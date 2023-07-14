125f49a61427da33b0faf0687a4e82afce94b799
murphymy04 <137734306+murphymy04@users.noreply.github.com>
2023-07-14 13:13:24 -0500

    gui updates

--- gui.py
+++ gui.py
@@ -75,40 +75,63 @@ class Window1:
     def __init__(self, master, diet_click, health_click, cuisine_click, meal_click, recipe_search):
         # creating window
         self.master = master
-        self.master.title("Recipy")
+        self.master.title("Pantry Pal")
         self.width= self.master.winfo_screenwidth()
         self.height= self.master.winfo_screenheight()
         self.master.geometry("%dx%d" % (self.width, self.height))
+
+        # main frame
+        self.main_frame = tk.Frame(self.master)
+        self.main_frame.pack(side=tk.TOP, pady=(self.height/4, 0))
+
+        # side frame
+        self.side_frame = tk.Frame(self.master)
+        self.side_frame.pack(side=tk.RIGHT, padx=(0, 50))
+
+        # title
+        self.title = tk.Label(self.main_frame, font=("Nexa", 30, "bold"), text="Pantry Pal")
+        self.title.pack(anchor="center", pady=(0, 25))
+
+        # image
+        self.attribution = tk.PhotoImage(file="transparent.png")
+        self.canvas = tk.Canvas(self.main_frame, height=50, width=50)
+        self.canvas.create_image(50, 50, image=self.attribution)
+        self.canvas.pack()
         
         # Entry bar
-        self.query = tk.Entry()
-        self.query.grid(row=1, column=0, columnspan=4)
+        self.query = tk.Entry(self.main_frame)
+        self.query.pack(side=tk.LEFT)
 
         # diet dropdown menu
         self.diet_choice = tk.StringVar(self.master)
-        self.diet_menu = tk.OptionMenu(self.master, self.diet_choice, *DIET_OPTIONS, command=diet_click)
-        self.diet_menu.grid(row=2, column=0)
+        self.diet_menu = tk.OptionMenu(self.side_frame, self.diet_choice, *DIET_OPTIONS, command=diet_click)
+        #self.diet_menu.grid(row=2, column=0)
+        self.diet_menu.pack()
 
         # health menu
         self.health_choice = tk.StringVar(self.master)
-        self.health_menu = tk.OptionMenu(self.master, self.health_choice, *HEALTH_OPTIONS, command=health_click)
-        self.health_menu.grid(row=2, column=1)
+        self.health_menu = tk.OptionMenu(self.side_frame, self.health_choice, *HEALTH_OPTIONS, command=health_click)
+        #self.health_menu.grid(row=2, column=1)
+        self.health_menu.pack()
 
         # cuisine menu
         self.cuisine_choice = tk.StringVar(self.master)
-        self.cuisine_menu = tk.OptionMenu(self.master, self.cuisine_choice, *CUISINE_OPTIONS, command=cuisine_click)
-        self.cuisine_menu.grid(row=2, column=2)
+        self.cuisine_menu = tk.OptionMenu(self.side_frame, self.cuisine_choice, *CUISINE_OPTIONS, command=cuisine_click)
+        #self.cuisine_menu.grid(row=2, column=2)
+        self.cuisine_menu.pack()
 
         # meal menu
         self.meal_choice = tk.StringVar(self.master)
-        self.meal_menu = tk.OptionMenu(self.master, self.meal_choice, *MEAL_OPTIONS, command=meal_click)
-        self.meal_menu.grid(row=2, column=3)
+        self.meal_menu = tk.OptionMenu(self.side_frame, self.meal_choice, *MEAL_OPTIONS, command=meal_click)
+        #self.meal_menu.grid(row=2, column=3)
+        self.meal_menu.pack()
 
         # search button
-        self.search_button = tk.Button(text="Go!", command=partial(self.Return_Query, recipe_search))
-        self.search_button.grid(row=1, column=5)
+        self.search_button = tk.Button(self.main_frame, text="Go!", command=partial(self.Return_Query, recipe_search))
+        #self.search_button.grid(row=1, column=5)
+        self.search_button.pack(side=tk.LEFT)
     
-    
+
     def Return_Query(self, recipe_search):
         recipe_search.query(input=self.query.get())
         recipe_search.search()
